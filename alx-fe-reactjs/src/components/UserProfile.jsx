fcb1a7d7938c68f1c056f09d5e96aad911b6c70d
codeRonin-gh <conradtagbor@gmail.com>
2025-07-11 17:53:09 +0000

    Update UserProfile.jsx

--- alx-fe-reactjs/src/components/UserProfile.jsx
+++ alx-fe-reactjs/src/components/UserProfile.jsx
@@ -1,13 +1,6 @@
 function UserProfile(props) {
   return (
-    <div style={{ 
-      border: '2px solid #ccc', 
-      padding: '1rem', 
-      borderRadius: '8px', 
-      maxWidth: '300px', 
-      margin: '2rem auto',
-      backgroundColor: '#f9f9f9' 
-    }}>
+    <div>
       <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
       <p>{props.bio}</p>
@@ -16,3 +9,4 @@ function UserProfile(props) {
 }
 
 export default UserProfile;
+
