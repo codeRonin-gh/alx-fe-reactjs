e8d1ffbf6d8ca81fadefd027b40278f9f3dda8ca
codeRonin-gh <conradtagbor@gmail.com>
2025-08-03 11:53:00 +0000

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 
-export async function fetchUserData({ username, location, minRepos }) {
+export async function fetchUserData(username) {
   try {
-    let query = "";
-
-    if (username) query += `${username}+`;
-    if (location) query += `location:${location}+`;
-    if (minRepos) query += `repos:>=${minRepos}`;
-
-    const url = `https://api.github.com/search/users?q=${encodeURIComponent(query.trim())}`;
-
+    const url = `https://api.github.com/users/${username}`;
     const response = await axios.get(url);
-
-    return response.data.items;
+    return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "GitHub API error");
   }
