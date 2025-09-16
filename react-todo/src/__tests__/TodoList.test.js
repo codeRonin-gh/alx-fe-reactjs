c454fcab499644c68a3e29bccc63a7742c6714fb
codeRonin-gh <conradtagbor@gmail.com>
2025-09-16 12:02:46 +0000

    Update TodoList.test.js

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,26 +1,26 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import TodoList from "./TodoList";
 
-test("renders TodoList heading", () => {
+test("renders heading", () => {
   render(<TodoList />);
-  expect(screen.getByText(/Todo List/i)).toBeInTheDocument();
+  expect(screen.getByText("Todo List")).toBeInTheDocument();
 });
 
-test("adds a todo item", () => {
+test("adds a todo", () => {
   render(<TodoList />);
-  fireEvent.change(screen.getByPlaceholderText(/Enter todo/i), {
+  fireEvent.change(screen.getByPlaceholderText("Enter todo"), {
     target: { value: "Buy milk" },
   });
-  fireEvent.click(screen.getByText(/Add/i));
-  expect(screen.getByText(/Buy milk/i)).toBeInTheDocument();
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("Buy milk")).toBeInTheDocument();
 });
 
-test("deletes a todo item", () => {
+test("deletes a todo", () => {
   render(<TodoList />);
-  fireEvent.change(screen.getByPlaceholderText(/Enter todo/i), {
+  fireEvent.change(screen.getByPlaceholderText("Enter todo"), {
     target: { value: "Buy milk" },
   });
-  fireEvent.click(screen.getByText(/Add/i));
-  fireEvent.click(screen.getByText(/Delete/i));
-  expect(screen.queryByText(/Buy milk/i)).not.toBeInTheDocument();
+  fireEvent.click(screen.getByText("Add"));
+  fireEvent.click(screen.getByText("Delete"));
+  expect(screen.queryByText("Buy milk")).not.toBeInTheDocument();
 });
