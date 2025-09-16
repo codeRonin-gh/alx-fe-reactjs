591426f05f841c184250a31875b738d8d26f1889
codeRonin-gh <conradtagbor@gmail.com>
2025-09-16 15:37:15 +0000

    Update TodoList.test.js

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import TodoList from "./TodoList";
 
-test("renders todo list", () => {
+test("renders todo list title", () => {
   render(<TodoList />);
   expect(screen.getByText("Todo List")).toBeInTheDocument();
+});
+
+test("renders default todos", () => {
+  render(<TodoList />);
   expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a project")).toBeInTheDocument();
 });
