ca15f7df7cb56d9be513948bca06816ba616e782
codeRonin-gh <conradtagbor@gmail.com>
2025-09-16 10:10:58 +0000

    Create App.jsx

---
+++ react-query-demo/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostsComponent from "./PostsComponent";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "20px" }}>
+      <h1>Advanced Data Handling with React Query</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
