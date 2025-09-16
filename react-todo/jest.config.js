aa66877e9fa882ce246399e91c8a4207fb2062ba
codeRonin-gh <conradtagbor@gmail.com>
2025-09-16 11:08:33 +0000

    Create jest.config.js

---
+++ react-todo/jest.config.js
@@ -0,0 +1,10 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
+};
