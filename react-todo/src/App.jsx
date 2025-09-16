e79c41387f64d42463f24340cce4febbea46f53d
codeRonin-gh <conradtagbor@gmail.com>
2025-09-16 15:33:31 +0000

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -3,8 +3,8 @@ import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div>
-      <h1>Todo Application</h1>
+    <div className="App">
+      <h1>My React App</h1>
       <TodoList />
     </div>
   );
