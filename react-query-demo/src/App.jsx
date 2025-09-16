2b44f93acceb64934ab2c35fcb7749feb1e8c297
codeRonin-gh <conradtagbor@gmail.com>
2025-09-16 10:07:48 +0000

    Create App.jsx

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from "./PostsComponent";
+
+// Create a query client instance
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div style={{ padding: 20 }}>
+        <h1>React Query Demo</h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
