7cbc74d19088c928e6fffb3d615b17467cd769c3
codeRonin-gh <conradtagbor@gmail.com>
2025-09-16 09:43:48 +0000

    Create Post.jsx

---
+++ react-router-advanced/components/pages/Post.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Post = () => {
+  const { id } = useParams();
+  return <h2>Post ID: {id}</h2>;
+};
+
+export default Post;
